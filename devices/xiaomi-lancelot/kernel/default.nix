5e78d098b3d1739929417498606e66a3683e18ca
highghlow <highghlow@proton.me>
2024-12-19 22:19:26 +0300

    Reverted to a time whem try_to_unmap had 2 args

--- devices/xiaomi-lancelot/kernel/default.nix
+++ devices/xiaomi-lancelot/kernel/default.nix
@@ -16,8 +16,8 @@ mobile-nixos.kernel-builder {
   src = fetchFromGitHub {
     owner = "MiCode";
     repo = "Xiaomi_Kernel_OpenSource";
-    rev = /*"d0b586e856dd638c73c34c3879536ca4ba73cd04";#*/"0f68431cdf9e396aa151ac7d764aab7d6eca7fc5";  #  branch lancelot-q-oss
-    sha256 = /*"sha256-Ym1ppHjRXH4p2IJw68fyx2zm+xdTEWKt+vh8HLYnavc=";#*/"sha256-wQWbReSLAOIwdVPu655ubqh60MUb/yjcVXy9BIUGwVc=";
+    rev = "d0b586e856dd638c73c34c3879536ca4ba73cd04";#"0f68431cdf9e396aa151ac7d764aab7d6eca7fc5";  #  branch lancelot-q-oss
+    sha256 = "sha256-Ym1ppHjRXH4p2IJw68fyx2zm+xdTEWKt+vh8HLYnavc=";#"sha256-wQWbReSLAOIwdVPu655ubqh60MUb/yjcVXy9BIUGwVc=";
   };
   patches = [
     ./yyloc.patch
